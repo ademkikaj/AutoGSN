a4ad3698496070f5491b239b037dc3546798fc0f
ademkikaj <ademkikaj95@gmail.com>
2023-04-15 23:04:34 +0200

    refactor

--- pytorch/utilities/convert.py
+++ pytorch/utilities/convert.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 import torch
 import torch_geometric, os, pickle, networkx as nx
 from torch_geometric.datasets import TUDataset
@@ -24,29 +25,58 @@ if "x" in tmp_graph:
     graph_node_feature = True
 if "edge_attr" in tmp_graph:
     graph_edge_feature = True
+
+node_counter = 0
 all_graphs = []
+
+
 for data in dataset:
     if graph_node_feature and graph_edge_feature:
-        g: Graph = torch_geometric.utils.to_networkx(data, to_undirected=True)
-        for index, node_attr in enumerate(data.x):
-            g.add_node(index, label=int(torch.argmax(node_attr)))
-        for i, edge in enumerate(g.edges):
-            g.add_edge(edge[0], edge[1], label=int(torch.argmax(data.edge_attr[i])))
+        g: Graph = torch_geometric.utils.to_networkx(
+            data, node_attrs=["x"], edge_attrs=["edge_attr"], to_undirected=True
+        )
+        for node in g.nodes:
+            g.add_node(
+                node, label=max(enumerate(g.nodes[node]["x"]), key=itemgetter(1))[0]
+            )
+        for edge in g.edges:
+            u = edge[0]
+            v = edge[1]
+            g.add_edge(
+                u,
+                v,
+                label=max(
+                    enumerate(g.get_edge_data(u, v)["edge_attr"]), key=itemgetter(1)
+                )[0],
+            )
         all_graphs.append(g)
     elif graph_node_feature:
-        g: Graph = torch_geometric.utils.to_networkx(data, to_undirected=True)
-        for index, node_attr in enumerate(data.x):
-            g.add_node(index, label=int(torch.argmax(node_attr)))
+        g: Graph = torch_geometric.utils.to_networkx(
+            data, node_attrs=["x"], to_undirected=True
+        )
+        for node in g.nodes:
+            g.add_node(
+                node, label=max(enumerate(g.nodes[node]["x"]), key=itemgetter(1))[0]
+            )
         for i, edge in enumerate(g.edges):
             g.add_edge(edge[0], edge[1], label=0)
         all_graphs.append(g)
     elif graph_edge_feature:
-        g: Graph = torch_geometric.utils.to_networkx(data, to_undirected=True)
+        g: Graph = torch_geometric.utils.to_networkx(
+            data, edge_attrs=["edge_attr"], to_undirected=True
+        )
         for node in g.nodes:
             g.add_node(node, label=0)
-        for i, edge in enumerate(g.edges):
-            g.add_edge(edge[0], edge[1], label=int(torch.argmax(data.edge_attr[i])))
-        all_graphs.append(g)
+        for edge in g.edges:
+            u = edge[0]
+            v = edge[1]
+            g.add_edge(
+                u,
+                v,
+                label=max(
+                    enumerate(g.get_edge_data(u, v)["edge_attr"]), key=itemgetter(1)
+                )[0],
+            )
     else:
         g: Graph = torch_geometric.utils.to_networkx(data, to_undirected=True)
         for node in g.nodes:
@@ -54,5 +84,7 @@ for data in dataset:
         for i, edge in enumerate(g.edges):
             g.add_edge(edge[0], edge[1], label=0)
         all_graphs.append(g)
+    node_counter += len(data.x)
+
 with open(data_save, "wb") as file:
     pickle.dump(all_graphs, file)
