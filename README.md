98ef810cd3f4bd16da1fcd6a54fbbb33d5b0510e
ademkikaj <ademkikaj95@gmail.com>
2023-04-15 21:00:06 +0200

    Refactor

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project includes three different subprojects:
 
 -   A frequent subgraph mining project based on [gSpan](https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=1184038&casa_token=hII_fVgAeycAAAAA:bc095z3FKW0KFzRYjy1yIcNp7jIKEy9jXd_9cv4FxosnXtFOkIHTkcy0cS8VJqrUK52z7aHIzw&tag=1)
 -   A discriminative subgraph mining project based on [GAIA](https://dl.acm.org/doi/pdf/10.1145/1807167.1807262?casa_token=G-mvu1_NCJgAAAAA:4ToovRUGv-i_YrmH8SxNQP15J3hTY-N0rq49oNgp1h45khNM6c3cgwcQjKGe66q-05jZeFXg8Kfr)
--   A `pytorch` environment that encodes features produced by `gSpan` and `GAIA` and trains on processed data
+-   A `pytorch` environment that encodes features (preprocesses graph data) produced by `gSpan` and `GAIA` and trains on processed data
 
 # How-to
 
@@ -15,9 +15,14 @@ This project includes three different subprojects:
 -   GAIA setup
     -   Download link and instruction of GAIA can be found [here](https://sourceforge.net/projects/discriminatives/)
 -   PyTorch Environment
-    -   Create a virtual environment `env` within `gnns` project by running `python3 -m venv env`.
+    -   Create a virtual environment `env` within `pytorch` project by running `python3 -m venv env`.
     -   Install packages by running `pip install -r requirements.txt`
 
 # Experiments
 
--   TUD Datasets to gSpan graphs
+-   Since we use TUD Benchmark, we need to convert TUD data to [Networkx](https://networkx.org/documentation/stable/index.html) data. To do so run the following command within the `pytorch` project
+    ```
+    # python utilities/convert.py -d {dataset_name}
+    # for example if we want to convert MUTAG to Networkx, run the following
+    python utilities/convert.py -d MUTAG
+    ```
--- pytorch/utilities/convert.py
+++ pytorch/utilities/convert.py
@@ -1,9 +1,13 @@
 import torch_geometric, os, pickle, networkx as nx
 from torch_geometric.datasets import TUDataset
 from networkx import Graph
-from plot import plot
+import argparse
 
-dataset = "NCI1"
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dataset", required=True)
+args = parser.parse_args()
+
+dataset = args.dataset
 data_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", dataset)
 data_save = os.path.join(
     os.path.dirname(os.path.dirname(__file__)), "data_networkx", dataset + ".dat"
@@ -11,25 +15,38 @@ data_save = os.path.join(
 
 dataset = TUDataset(root=data_root, name=dataset)
 
+tmp_graph = dataset[0]
+graph_node_feature = False
+graph_edge_feature = False
+
+if "x" in tmp_graph:
+    graph_node_feature = True
+if "edge_attr" in tmp_graph:
+    graph_edge_feature = True
+
 all_graphs = []
 
 for data in dataset:
-    try:
+    g: Graph = None
+    if graph_node_feature and graph_edge_feature:
         g: Graph = torch_geometric.utils.to_networkx(
             data,
             node_attrs=["x"],
             edge_attrs=["edge_attr"],
             to_undirected=True,
         )
-    except:
+    elif graph_node_feature:
         g: Graph = torch_geometric.utils.to_networkx(
             data,
             node_attrs=["x"],
             to_undirected=True,
         )
-    # print(nx.get_node_attributes(g, "x"))
-    # print(g.nodes(data=True)[0])
-    # print(nx.get_edge_attributes(g, "edge_attr"))
+    elif graph_edge_feature:
+        g: Graph = torch_geometric.utils.to_networkx(
+            data,
+            edge_attrs=["edge_attr"],
+            to_undirected=True,
+        )
     all_graphs.append(g)
 print(len(all_graphs))
 with open(data_save, "wb") as file:
