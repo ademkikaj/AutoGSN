e0755d18938af5cd33f73212572d872cdfa28d25
ademkikaj <ademkikaj95@gmail.com>
2023-04-18 18:53:53 +0200

    refactor

--- gspan/mutag_playground/selection.py
+++ gspan/mutag_playground/selection.py
@@ -1,3 +1,4 @@
+import argparse
 import sklearn.feature_selection as fs
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -5,19 +6,86 @@ import pickle
 import os
 import networkx as nx
 
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-dataset = "MUTAG_labeled_nr"
-dataset_labels = "MUTAG_y"
-features = "MUTAG_4"
+
+def pickle_features(dataset, features):
+    selected_features = os.path.join(os.path.dirname(__file__), dataset + "_.dat")
+    pickle.dump(features, open(selected_features, "wb"))
 
 
-def save_features(file_name, features_df: pd.DataFrame):
-    features_df.to_pickle(
-        os.path.join(
-            os.path.dirname(__file__),
-            file_name + "_df.dat",
-        )
+def load_df(dataset):
+    return pickle.load(open(dataset, "rb"))
+
+
+def chi_square_features(train, labels, top_k, features_graph):
+    chi_scores = fs.chi2(train, labels)
+    p_values = pd.Series(chi_scores[1], index=train.columns)
+    p_values.sort_values(ascending=True, inplace=True)
+    p_values = p_values[-top_k:]
+    selected_graphs = []
+    for key in p_values.keys():
+        # print(key)
+        selected_graphs.append(features_graph[key])
+    return selected_graphs
+
+
+def random_forest_features(train, label, top_k, features_graph):
+    forest = RandomForestClassifier(random_state=0)
+    forest.fit(train, label)
+    feature_names = [f"{i}" for i in range(train.shape[1])]
+    importances = forest.feature_importances_
+    forest_importances = pd.Series(importances, index=feature_names)
+    forest_importances.sort_values(ascending=True, inplace=True)
+    forest_importances = forest_importances[-top_k:]
+    selected_graphs = []
+    for key in list(forest_importances.keys()):
+        selected_graphs.append(features_graph[int(key)])
+    return selected_graphs
+
+
+def xgb_features(train, label, top_k, features_graph):
+    test_size = 0.20
+    X_train, X_test, y_train, y_test = train_test_split(
+        train, label, test_size=test_size, random_state=7
     )
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+    # plot_importance(model)
+    # plt.show()
+    y_pred = model.predict(X_test)
+    predictions = [round(value) for value in y_pred]
+    accuracy = accuracy_score(y_test, predictions)
+    # print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    feature_important = model.get_booster().get_score(importance_type="weight")
+    keys = list(feature_important.keys())
+    values = list(feature_important.values())
+    data = pd.DataFrame(data=values, index=keys, columns=["score"]).sort_values(
+        by="score", ascending=False
+    )
+    # print(data.head(top_k))
+    selected_graphs = []
+    for key in list(data.index[:top_k]):
+        selected_graphs.append(features_graph[int(key)])
+    return selected_graphs
+
+
+def mutual_info(train, label, top_k, features_graph):
+    mutual = fs.mutual_info_classif(train, label)
+    mutual = pd.Series(mutual)
+    mutual.sort_values(ascending=False, inplace=True)
+    selected_graphs = []
+    for key in mutual[:top_k].keys():
+        # print(key)
+        selected_graphs.append(features_graph[key])
+    return selected_graphs
+
+
+def save_features(flatten_graph, features_df: pd.DataFrame):
+    features_df.to_pickle(flatten_graph)
 
 
 def load_labels(dataset):
@@ -29,7 +97,7 @@ def load_labels(dataset):
     return pickle.load(open(graph_labels, "rb"))
 
 
-def load_dataset(dataset):
+def load_all_graphs(dataset):
     graphs = os.path.join(
         os.path.dirname(__file__),
         dataset + ".dat",
@@ -82,30 +150,68 @@ def subgraph_exists(graph, subgraph):
     ).subgraph_is_isomorphic()
 
 
+def perform_selection(
+    dataframe_name, df, feature_selection_input, top_k_features, graph_features
+):
+    train = df.drop("y", axis=1)
+    label = df["y"]
+    if feature_selection_input == "xgb":
+        res = xgb_features(train, label, top_k_features, graph_features)
+    elif feature_selection_input == "rf":
+        res = random_forest_features(train, label, top_k_features, graph_features)
+    elif feature_selection_input == "chi2":
+        res = chi_square_features(train, label, top_k_features, graph_features)
+    elif feature_selection_input == "mi":
+        res = mutual_info(train, label, top_k_features, graph_features)
+
+    pickle_features(
+        dataframe_name + "_" + feature_selection_input + "_" + top_k_features, res
+    )
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dataset", required=True)
+parser.add_argument("-f", "--features", required=True)
+parser.add_argument(
+    "-s", "--selection", required=True, choices=["xgb", "rf", "chi2", "mi"]
+)
+parser.add_argument("-k", "--k_features", required=True)
+args = parser.parse_args()
+
+dataset_name = args.dataset
+dataset_labels = dataset_name + "_target"
+features = args.features
+
 graph_features = load_graph_features(features)
 print("Loaded %d features" % len(graph_features))
-dataset = load_dataset(dataset)
+graph_dataset = load_all_graphs(dataset_name)
 dataset_labels = load_labels(dataset_labels)
-df = {}
-for k, graph in enumerate(dataset):
-    print("%d/%d" % (k, len(dataset)))
-    for i, subgraph in enumerate(graph_features):
-        try:
-            df[i].append(int(subgraph_exists(graph, subgraph)))
-        except:
-            df[i] = [int(subgraph_exists(graph, subgraph))]
 
-df["y"] = dataset_labels
+dataframe_name = dataset_name + "_" + features + "_flatten_df.dat"
 
-df = pd.DataFrame(df)
-save_features(features, df)
+# check if flatten dataframe exists
+flatten_graph = os.path.join(os.path.dirname(__file__), dataframe_name)
 
-print(df.head())
-x = df.drop("y", axis=1)
-y = df["y"]
-chi_scores = fs.chi2(x, y)
+if not os.path.exists(flatten_graph):
+    df = {}
+    for k, graph in enumerate(graph_dataset):
+        print("%d/%d" % (k + 1, len(graph_dataset)))
+        for i, subgraph in enumerate(graph_features):
+            try:
+                df[i].append(int(subgraph_exists(graph, subgraph)))
+            except:
+                df[i] = [int(subgraph_exists(graph, subgraph))]
 
-p_values = pd.Series(chi_scores[1])
-p_values.sort_values(ascending=False, inplace=True)
-p_values.plot.bar()
-plt.show()
+    df["y"] = dataset_labels
+
+    df = pd.DataFrame(df)
+
+    save_features(flatten_graph, df)
+    perform_selection(
+        dataframe_name, df, args.selection, args.k_features, graph_features
+    )
+else:
+    df = load_df(flatten_graph)
+    perform_selection(
+        dataframe_name, df, args.selection, args.k_features, graph_features
+    )
